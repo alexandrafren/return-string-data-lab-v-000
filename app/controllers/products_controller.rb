431dd48403e8096a7ee93fed96e3ef0532e85b46
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-25 17:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,11 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def body
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :price, :description, :inventory)
